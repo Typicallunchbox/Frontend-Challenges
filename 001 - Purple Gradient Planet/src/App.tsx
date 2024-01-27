e342d07b4bae7c0b3876a45b7420e030a3063340
Keegan <keegan.launspach1@hotmail.com>
2024-01-27 08:11:53 +0200

    added hover effect

--- 001 - Purple Gradient Planet/src/App.tsx
+++ 001 - Purple Gradient Planet/src/App.tsx
@@ -2,12 +2,14 @@ import './style.scss';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Invalid from "./pages/Invalid";
+import { Navbar } from './components';
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Invalid />} />
--- 001 - Purple Gradient Planet/src/components/Navbar/index.tsx
+++ 001 - Purple Gradient Planet/src/components/Navbar/index.tsx
@@ -1,6 +1,20 @@
+import { Link, useNavigate } from "react-router-dom"
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+        <Link to={'/'}>Home</Link>
+        <Link to={'/'}>About</Link>
+        <Link to={'/'}>Contact</Link>
+
+        <h2 onClick={() => navigate('/')}>MetaBrass</h2>
+
+        <Link to={'/'}>Galaxies</Link>
+        <Link to={'/'}>Solar System</Link>
+        <Link to={'/'}>Earth</Link>
+        <h2 className="mobile-menu" onClick={() => navigate('/')}>Menu</h2>
+    </div>
   )
 }
 
